9ac45c986f8416220f083258cd4d541463a13851
FernanCastillo <fern.castillop@duocuc.cl>
2026-04-01 20:20:24 -0300

    Se agregan los grupos de seguridad

--- infra/security_groups.tf
+++ infra/security_groups.tf
@@ -1,5 +1,5 @@
-resource "aws_security_group" "web_sg" {
-  name = "inovatech"
+resource "aws_security_group" "frontend_sg" {
+  name = "inovatech-frontend"
 
   ingress {
     from_port = 80
@@ -23,3 +23,55 @@ resource "aws_security_group" "web_sg" {
   }
 
 }
+
+resource "aws_security_group" "backend_sg" {
+  name = "inovatech-backend"
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.frontend_sg.id]
+  }
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "db_sg" {
+  name = "inovatech-db"
+
+  ingress {
+    from_port       = 3306
+    to_port         = 3306
+    protocol        = "tcp"
+    security_groups = [aws_security_group.backend_sg.id]
+  }
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+#Preguntar si es necesario que el SSH esté abierto en todos los grupos de seguridad
