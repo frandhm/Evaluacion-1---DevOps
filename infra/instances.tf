7111913c06b8679ae89cf972cb2e38859b5c57fe
FernanCastillo <fern.castillop@duocuc.cl>
2026-04-01 20:14:53 -0300

    Se modifican las instancias del proyecto

--- infra/instances.tf
+++ infra/instances.tf
@@ -1,41 +1,73 @@
-resource "aws_instance" "inovatech_server" {
+resource "aws_instance" "inovatech_frontend" {
   ami = "ami-0c02fb55956c7d316"
   instance_type = "t2.micro"
 
   key_name = "inovatech-key"
 
-  security_groups = [aws_security_group.web_sg.name]
+  vpc_security_group_ids = [aws_security_group.frontend_sg.id]
+
+  subnet_id = aws_subnet.public.id
 
   user_data = <<-EOF
                 #!/bin/bash
                 apt update -y
-                apt install nginx -y
+                apt upgrade -y
+                apt install nginx docker.io -y
                 systemctl start nginx
                 systemctl enable nginx
+                systemctl start docker
+                systemctl enable docker
                 EOF
 
     tags = {
-        name = "inovatech-frontend"
+        Name = "inovatech-frontend"
     }
 }
 
-resource "aws_instance" "inovatech_server" {
+resource "aws_instance" "inovatech_backend" {
   ami = "ami-0c02fb55956c7d316"
   instance_type = "t2.micro"
 
   key_name = "inovatech-key"
 
-  security_groups = [aws_security_group.web_sg.name]
+  vpc_security_group_ids = [aws_security_group.backend_sg.id]
+
+  subnet_id = aws_subnet.private_app.id
 
   user_data = <<-EOF
                 #!/bin/bash
                 apt update -y
-                apt install nginx -y
-                systemctl start nginx
-                systemctl enable nginx
+                apt upgrade -y
+                apt install docker.io -y
+                systemctl start docker
+                systemctl enable docker
                 EOF
 
     tags = {
-        name = "inovatech-backend"
+        Name = "inovatech-backend"
+    }
+}
+
+resource "aws_instance" "inovatech_database" {
+  ami = "ami-0c02fb55956c7d316"
+  instance_type = "t2.micro"
+
+  key_name = "inovatech-key"
+
+  vpc_security_group_ids = [aws_security_group.db_sg.id]
+
+  subnet_id = aws_subnet.private_app.id
+
+  user_data = <<-EOF
+                #!/bin/bash
+                apt update -y
+                apt upgrade -y
+                apt install mysql-server -y
+                systemctl start mysql
+                systemctl enable mysql
+                EOF
+
+    tags = {
+        Name = "inovatech-database"
     }
 }
